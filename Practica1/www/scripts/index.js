a0404917a57e5868698dc2ee4580525d3670fde3
Atruo <33220763+atruo@users.noreply.github.com>
2018-09-27 16:47:03 +0200

    implementando el boton de las tablas.

--- Practica1/www/scripts/index.js
+++ Practica1/www/scripts/index.js
@@ -1,4 +1,7 @@
-﻿function datosMun50000(){
+﻿var lat = 35.027222;
+var long = -111.0225;
+
+function datosMun50000() {
     var datos;
     var datosfiltrados2 = [];
     var key = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJiYW55dWxzOThAZ21haWwuY29tIiwianRpIjoiYzQ3YzgyMzMtYzhjOC00OWQ5LTk0NzYtMDg2ZTczZGNmNDBjIiwiaXNzIjoiQUVNRVQiLCJpYXQiOjE1Mzc5NDcwMDksInVzZXJJZCI6ImM0N2M4MjMzLWM4YzgtNDlkOS05NDc2LTA4NmU3M2RjZjQwYyIsInJvbGUiOiIifQ.1Km6vaOtp-mmugfFkPhDYxziK_MZGdCAZG71Mi1ibJw';
@@ -50,7 +53,8 @@
             }
     
 }
-function estaciones() {
+function estaciones() {//Tabla de las estaciones meteorologicas de espana
+    //Hay que hacer dos gets porque el primero nos da una una url con el JSON
 
     var datos;
     var datosfiltrados2 = [];
@@ -82,7 +86,7 @@ function estaciones() {
             $.ajax(settings).done(function (response) {
                 var datos = JSON.parse(response);
                 
-                tabla = $('#estaciones').DataTable({
+                tabla = $('#estaciones').DataTable({//Crear Tabla de estaciones
                     "data": datos,
                     "columns": [
                         {
@@ -96,9 +100,33 @@ function estaciones() {
                         },
                         {
                             "data": "indicativo"
+                        },
+                        {
+                            "defaultContent": "<button class = 'open'>Mapa</button>"
                         }
                         
                     ]
+                });
+                tabla.on('click', 'button', function () {//Lo que realiza el boton de cada columna
+                    var modal = document.getElementById('myModal');
+                    var mapa = document.getElementById('mapa');
+                    var span = document.getElementsByClassName("close")[0];
+                    GetMap();
+                    alert('Row index: ' + tabla.row(this).index());
+                 
+                    lat = 35.027222;
+                    long = -111.0225;
+                    modal.style.display = "block";
+                    span.onclick = function () {
+                        modal.style.display = "none";
+                    }
+                    window.onclick = function (event) {
+                        if (event.target == modal) {
+                            modal.style.display = "none";
+                        }
+                    }
+
+
                 });
             });
             
@@ -107,3 +135,19 @@ function estaciones() {
     }
 
 }
+
+
+function GetMap() {
+ 
+    var map = new Microsoft.Maps.Map(document.getElementById('mapa'), { credentials: 'ArapQL_90y_pC9SInxjpAygl3kIgXJomDja0LTww44lc2vVc_i2HBXl_1SSXBpmp'});
+    map.setView({
+        mapTypeId: Microsoft.Maps.MapTypeId.aerial,
+        center: new Microsoft.Maps.Location(lat, long),
+        zoom: 15
+    });
+    console.log(lat+'/'+long)
+    
+
+    
+}
+    
