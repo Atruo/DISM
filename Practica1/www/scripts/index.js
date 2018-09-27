85b16db9d91f2bae7f05754bbb6cf769b659f748
Atruo <33220763+atruo@users.noreply.github.com>
2018-09-27 13:54:56 +0200

    estaciones sin el añadido

--- Practica1/www/scripts/index.js
+++ Practica1/www/scripts/index.js
@@ -1,8 +1,4 @@
-﻿function mun50000() {
-
-    location.href = './mun50000.html';
-}
-function datosMun50000(){
+﻿function datosMun50000(){
     var datos;
     var datosfiltrados2 = [];
     var key = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJiYW55dWxzOThAZ21haWwuY29tIiwianRpIjoiYzQ3YzgyMzMtYzhjOC00OWQ5LTk0NzYtMDg2ZTczZGNmNDBjIiwiaXNzIjoiQUVNRVQiLCJpYXQiOjE1Mzc5NDcwMDksInVzZXJJZCI6ImM0N2M4MjMzLWM4YzgtNDlkOS05NDc2LTA4NmU3M2RjZjQwYyIsInJvbGUiOiIifQ.1Km6vaOtp-mmugfFkPhDYxziK_MZGdCAZG71Mi1ibJw';
@@ -53,4 +49,61 @@ function datosMun50000(){
                 });
             }
     
+}
+function estaciones() {
+
+    var datos;
+    var datosfiltrados2 = [];
+    var key = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJiYW55dWxzOThAZ21haWwuY29tIiwianRpIjoiYzQ3YzgyMzMtYzhjOC00OWQ5LTk0NzYtMDg2ZTczZGNmNDBjIiwiaXNzIjoiQUVNRVQiLCJpYXQiOjE1Mzc5NDcwMDksInVzZXJJZCI6ImM0N2M4MjMzLWM4YzgtNDlkOS05NDc2LTA4NmU3M2RjZjQwYyIsInJvbGUiOiIifQ.1Km6vaOtp-mmugfFkPhDYxziK_MZGdCAZG71Mi1ibJw';
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://opendata.aemet.es/opendata/api/valores/climatologicos/inventarioestaciones/todasestaciones?api_key=" + key,
+        "method": "GET",
+        "headers": {
+            "cache-control": "no-cache"
+        }
+    }
+
+
+    InicializarGrid();
+ 
+    function InicializarGrid() {
+        $.ajax(settings).done(function (response) {            
+            settings = {
+                "async": true,
+                "crossDomain": true,
+                "url": response.datos,
+                "method": "GET",
+                "headers": {
+                    "cache-control": "no-cache"
+                }
+            }
+            $.ajax(settings).done(function (response) {
+                var datos = JSON.parse(response);
+                
+                tabla = $('#estaciones').DataTable({
+                    "data": datos,
+                    "columns": [
+                        {
+                            "data": "nombre"
+                        },
+                        {
+                            "data": "latitud"
+                        },
+                        {
+                            "data": "longitud"
+                        },
+                        {
+                            "data": "indicativo"
+                        }
+                        
+                    ]
+                });
+            });
+            
+            
+        });
+    }
+
 }
